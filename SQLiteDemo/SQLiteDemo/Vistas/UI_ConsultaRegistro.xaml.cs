fd12c25700d3a89ab8422f54dbb6ab9ab8eb7690
David Velez <dvelezmesa@gmail.com>
2019-05-27 01:08:05 -0500

    PrimerComit

---
+++ SQLiteDemo/SQLiteDemo/Vistas/UI_ConsultaRegistro.xaml.cs
@@ -0,0 +1,48 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SQLiteDemo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class UI_ConsultaRegistro : ContentPage
+	{
+        private SQLiteAsyncConnection _conn;
+        private ObservableCollection<T_Registro> _TablaRegistro;
+        public UI_ConsultaRegistro ()
+		{
+			InitializeComponent ();
+            _conn = DependencyService.Get<ISQLiteDB>().GetConnection();
+            NavigationPage.SetHasBackButton(this,false);
+        }
+        protected async override void OnAppearing()
+        {
+            var ResulRegistros = await _conn.Table<T_Registro>().ToListAsync();
+            _TablaRegistro = new ObservableCollection<T_Registro>(ResulRegistros);
+            ListaUsuarios.ItemsSource = _TablaRegistro;
+            base.OnAppearing();
+        }
+        void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            var Obj = (T_Registro)e.SelectedItem;
+            var item = Obj.Id.ToString();
+            int ID = Convert.ToInt32(item);
+            try
+            {
+                Navigation.PushAsync(new UI_Elemento(ID));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ SQLiteDemo/SQLiteDemo/Vistas/UI_Elemento.xaml.cs
@@ -0,0 +1,53 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SQLiteDemo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class UI_Elemento : ContentPage
+	{
+        public int IdSeleccionado;
+        private SQLiteAsyncConnection _conn;
+        IEnumerable<T_Registro> ResultadoDelete;
+        IEnumerable<T_Registro> ResultadoUpdate;
+        public UI_Elemento (int id)
+		{
+			InitializeComponent ();
+            _conn = DependencyService.Get<ISQLiteDB>().GetConnection();
+            IdSeleccionado = id;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            mensaje.Text = "Se afectará al Gasto ["+IdSeleccionado+"]";
+        }
+        private void btn_actualizar(object sender,EventArgs e)
+        {
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "WalletEasy.db");
+            var db = new SQLiteConnection(databasePath);
+            ResultadoUpdate =Update(db,Nombre.Text,Usuario.Text,Contrasenia.Text,IdSeleccionado);
+        }
+        private void btn_eliminar(object sender, EventArgs e)
+        {
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "WalletEasy.db");
+            var db = new SQLiteConnection(databasePath);
+            ResultadoDelete = Delete(db,IdSeleccionado);
+        }
+        public static IEnumerable<T_Registro> Delete(SQLiteConnection db, int id)
+        {
+            return db.Query<T_Registro>("DELETE FROM T_Registro where Id = ?", id);
+        }
+        public static IEnumerable<T_Registro> Update(SQLiteConnection db, string nombre,string usuario,string contrasenia,int id)
+        {
+            return db.Query<T_Registro>("UPDATE T_Registro SET Nombre = ?, Usuario = ?, Contrasenia = ? where Id = ?",nombre,usuario,contrasenia, id);
+        }
+    }
+}
